d37218a87bd6483a7514e0a96c17692357dd061f
Celeo <celeodor@gmail.com>
2017-07-19 21:28:37 -0700

    Progress

--- pycord/main.py
+++ pycord/main.py
@@ -10,51 +10,63 @@ class Pycord:
     url_base = 'https://discordapp.com/api/'
     version = '0.0.1'
 
-    def __init__(self, client_id: str, token: str, user_agent: str=None) -> None:
-        self.client_id = client_id
+    def __init__(self, token: str, user_agent: str=None) -> None:
         self.token = token
         self.user_agent = user_agent or f'Pycord (github.com/Celeo/Pycord, {Pycord.version})'
         self.__setup_logger()
 
     def __setup_logger(self):
         self.logger = logging.getLogger('discord')
-        self.logger.setLevel(20)
+        self.logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter(style='{', fmt='{asctime} [{levelname}] {message}', datefmt='%Y-%m-%d %H:%M:%S')
         handler = logging.StreamHandler(sys.stdout)
         handler.setFormatter(formatter)
-        handler.setLevel(20)
+        handler.setLevel(logging.DEBUG)
         self.logger.addHandler(handler)
-        handler = logging.FileHandler(20)
+        handler = logging.FileHandler('pycord.log')
         handler.setFormatter(formatter)
-        handler.setLevel(20)
+        handler.setLevel(logging.DEBUG)
         self.logger.addHandler(handler)
 
     def __build_headers(self) -> Dict[str, str]:
         return {
             'Authorization': f'Bot {self.token}',
-            'User-Agent': self.user_agent
+            'User-Agent': self.user_agent,
+            'Content-Type': 'application/json'
         }
 
-    def __query(self, path: str) -> Dict[str, Any]:
+    def __get(self, path: str) -> Dict[str, Any]:
         url = Pycord.url_base + path
-        self.logger.debug(f'Making request to {url}')
+        self.logger.debug(f'Making GET request to "{url}"')
         r = requests.get(url, headers=self.__build_headers())
-        self.logger.debug(f'Response from {url} was {r.status_code}')
+        self.logger.debug(f'GET response from "{url}" was "{r.status_code}"')
         if r.status_code != 200:
-            raise ValueError(f'Non-200 response from Discord API ({r.status_code}): {r.text}')
+            raise ValueError(f'Non-200 GET response from Discord API ({r.status_code}): {r.text}')
+        return r.json()
+
+    def __post(self, path: str, data: Dict[str, Any]) -> Dict[str, Any]:
+        url = Pycord.url_base + path
+        self.logger.debug(f'Making POST request to "{url}"')
+        r = requests.post(url, headers=self.__build_headers(), json=data)
+        self.logger.debug(f'POST response from "{url}" was "{r.status_code}"')
+        if r.status_code != 200:
+            raise ValueError(f'Non-200 POST response from Discord API ({r.status_code}): {r.text}')
         return r.json()
 
     def get_basic_bot_info(self) -> Dict[str, Any]:
-        return self.__query('users/@me')
+        return self.__get('users/@me')
 
-    def get_connected_guilds(self):
-        pass
+    def get_connected_guilds(self) -> Dict[str, Any]:
+        return self.__get('users/@me/guilds')
 
-    def get_guild_info(self, id):
-        pass
+    def get_guild_info(self, id: str) -> Dict[str, Any]:
+        return self.__get(f'guilds/{id}')
 
-    def get_channels_in(self, guild_id):
-        pass
+    def get_channels_in(self, guild_id: str) -> Dict[str, Any]:
+        return self.__get(f'guilds/{guild_id}/channels')
 
-    def get_channel_info(self, id):
-        pass
+    def get_channel_info(self, id: str) -> Dict[str, Any]:
+        return self.__get(f'channels/{id}')
+
+    def send_message(self, id: str, message: str):
+        return self.__post(f'channels/{id}/messages', {'content': message})
